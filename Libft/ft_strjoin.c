72d8fbc689a5634fd7f82f2768d112cb236f6525
emlara-va <emiliolaravargas@hotmail.com>
2025-05-29 16:29:13 +0200

    .

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: elara-va <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:07:29 by elara-va          #+#    #+#             */
-/*   Updated: 2025/05/02 16:48:21 by elara-va         ###   ########.fr       */
+/*   Updated: 2025/05/25 18:12:14 by elara-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	s3 = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	s3 = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!s3)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (s1[i])
-	{
-		s3[j] = s1[i];
-		i++;
-		j++;
-	}
+		s3[j++] = s1[i++];
 	i = 0;
 	while (s2[i])
 		s3[j++] = s2[i++];
